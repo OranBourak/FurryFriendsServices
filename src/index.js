1be9c68f6b5e9a8f2c54a8ae837df459bc7bc9d5
VladFearo <69718459+VladFearo@users.noreply.github.com>
2023-08-18 17:51:34 +0300

    added lint

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import  Express  from "express";
-
-const app = Express();
+import Express from "express";
+const app = new Express();
 
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+    res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log("Example app listening on port 3000!");
+    console.log("Example app listening on port 3000!");
 });
