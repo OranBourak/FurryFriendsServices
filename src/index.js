cb340d5b5cbcf81e5aab278fa075e4faa44b5433
VladFearo <69718459+VladFearo@users.noreply.github.com>
2023-08-18 14:30:24 +0300

    simple server and nodemon added

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import  Express  from "express";
 
 const app = Express();
 
+const port = process.env.PORT || 3000;
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("Example app listening on port 3000!");
 });
