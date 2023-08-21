1fd5352f21c9dbe8e9f388a5632d03c416e318ed
TomerBurman <93316511+TomerBurman@users.noreply.github.com>
2023-08-21 22:24:59 +0200

    added Login page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 import Express from "express";
-const app = new Express();
+
 
 const port = process.env.PORT || 3000;
+const app =new Express();
+app.use(Express.json()); // Parse JSON request bodies
+app.use(Express.urlencoded({ extended: true })); // Parse URL-encoded form data
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+app.post('/login', (req, res) => {
+  const formData = req.body;
+  console.log(formData);
+  // Validate and process the formData
+  
+  // Send a response back to the client
+  res.status(200).json({ message: 'Form submitted successfully' });
 });
 
 app.listen(port, () => {
-    console.log("Example app listening on port 3000!");
+  console.log(`Server is running on port ${port}`);
 });
+
+// const app = new Express();
+
+// const port = process.env.PORT || 3000;
+
+// app.get("/", (req, res) => {
+//     res.send("Hello World!");
+// });
+
+// app.get("/login", (req, res) => {
+//     console.log('Hello world!');
+// });
+
+
+
+
+// app.listen(port, () => {
+//     console.log("Example app listening on port 3000!");
+// });
