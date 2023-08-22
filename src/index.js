9bcaf45efe4522d046124cf0274fe41a2404f953
VladFearo <69718459+VladFearo@users.noreply.github.com>
2023-08-22 16:11:17 +0300

    psuedo db added

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import cors from "cors";
 import fs from "fs";
 
 const port = process.env.PORT || 5000;
-const app =new Express();
+const app = new Express();
 app.use(Express.json()); // Parse JSON request bodies
 app.use(Express.urlencoded({ extended: true })); // Parse URL-encoded form data
 app.use(cors());
 
 function checkLogin(email, password) {
 
-  const rawData = fs.readFileSync('../DB/users.json');
+  const rawData = fs.readFileSync('./DB/users.json');
 
   const data = JSON.parse(rawData);
   console.log(data);
@@ -27,36 +27,28 @@ function checkLogin(email, password) {
   return false;
 }
 
+
+
+
+
 app.post('/login', (req, res) => {
   const formData = req.body;
   console.log(formData);
-  res.status(200).json({ message: 'Form submitted successfully' });
-  // {if (checkLogin(formData.email, formData.password))
-  //   res.status(200).json({ message: 'Form submitted successfully' });
-  // else 
-  //   res.status(401).json({ message: 'Invalid credentials' });
-  // }
+  const test_login = checkLogin(formData.email, formData.password);
+  {
+    if (test_login)
+      res.status(200).json({ message: 'Form submitted successfully' });
+    else
+      res.status(401).json({ message: 'Invalid credentials' });
+  }
 });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-// const app = new Express();
-
-// const port = process.env.PORT || 3000;
-
-// app.get("/", (req, res) => {
-//     res.send("Hello World!");
-// });
-
-// app.get("/login", (req, res) => {
-//     console.log('Hello world!');
-// });
 
 
 
 
-// app.listen(port, () => {
-//     console.log("Example app listening on port 3000!");
-// });
+
