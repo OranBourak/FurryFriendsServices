458dee01b8bc9f954b7d83960b8638e5e1eddfb8
Lior <Shilon199707@gmail.com>
2023-08-22 17:02:32 +0200

    user name to fronend

--- src/index.js
+++ src/index.js
@@ -5,50 +5,43 @@ import fs from "fs";
 const port = process.env.PORT || 5000;
 const app = new Express();
 app.use(Express.json()); // Parse JSON request bodies
-app.use(Express.urlencoded({ extended: true })); // Parse URL-encoded form data
+app.use(Express.urlencoded({extended: true})); // Parse URL-encoded form data
 app.use(cors());
 
+/**
+ * checks if the user exists in the db
+ *
+ * @param {string} email - user's email.
+ * @param {string} password - user's password.
+ * @return {string} The name of the user.
+ */
 function checkLogin(email, password) {
+    const rawData = fs.readFileSync("./DB/users.json");
 
-  const rawData = fs.readFileSync('./DB/users.json');
+    const data = JSON.parse(rawData);
 
-  const data = JSON.parse(rawData);
-  console.log(data);
-
-  for (const user of data) {
-
-    if (user.email === email && user.password === password) {
-
-      return true;
-
+    for (const user of data) {
+        if (user.email === email && user.password === password) {
+            return user.name;
+        }
     }
-
-  }
-  return false;
+    return "";
 }
 
-
-
-
-
-app.post('/login', (req, res) => {
-  const formData = req.body;
-  console.log(formData);
-  const test_login = checkLogin(formData.email, formData.password);
-  {
-    if (test_login)
-      res.status(200).json({ message: 'Form submitted successfully' });
-    else
-      res.status(401).json({ message: 'Invalid credentials' });
-  }
+app.post("/login", (req, res) => {
+    const formData = req.body;
+    console.log("body: ", formData);
+    const userName = checkLogin(formData.email, formData.password);
+    {
+        if (userName) {
+            res
+                .status(200)
+                .json({message: "Form submitted successfully ",
+                    name: userName});
+        } else res.status(401).json({message: "Invalid credentials"});
+    }
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
-
-
-
-
-
-
