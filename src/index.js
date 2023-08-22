40e9fc39eba3fb7e3a842c455d5887f66783c809
TomerBurman <93316511+TomerBurman@users.noreply.github.com>
2023-08-22 22:49:41 +0200

    sprint-1 details added

--- src/index.js
+++ src/index.js
@@ -8,40 +8,37 @@ app.use(Express.json()); // Parse JSON request bodies
 app.use(Express.urlencoded({extended: true})); // Parse URL-encoded form data
 app.use(cors());
 
-/**
- * checks if the user exists in the db
- *
- * @param {string} email - user's email.
- * @param {string} password - user's password.
- * @return {string} The name of the user.
- */
 function checkLogin(email, password) {
     const rawData = fs.readFileSync("./DB/users.json");
 
     const data = JSON.parse(rawData);
+    console.log(data);
 
     for (const user of data) {
         if (user.email === email && user.password === password) {
-            return user.name;
+            return true;
         }
     }
-    return "";
+    return false;
 }
+
 
 app.post("/login", (req, res) => {
     const formData = req.body;
-    console.log("body: ", formData);
-    const userName = checkLogin(formData.email, formData.password);
+    console.log(formData);
+    const testLogin = checkLogin(formData.email, formData.password);
     {
-        if (userName) {
-            res
-                .status(200)
-                .json({message: "Form submitted successfully ",
-                    name: userName});
-        } else res.status(401).json({message: "Invalid credentials"});
+        if (testLogin) {
+            res.status(200).json({message: "Form submitted successfully"});
+        } else {
+            res.status(401).json({message: "Invalid credentials"});
+        }
+      
     }
 });
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+
