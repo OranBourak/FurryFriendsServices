cf3e5f8566155349cd437b0c93044aeff81056fc
Lior <Shilon199707@gmail.com>
2023-09-07 14:46:20 +0200

    (ADD) Connection to Mongo DB

---
+++ src/services/database/db.js
@@ -0,0 +1,24 @@
+/* eslint-disable object-curly-spacing */
+/* eslint-disable no-tabs */
+/* eslint-disable indent */
+const mongoose = require("mongoose");
+
+const config = require("../../config/config");
+const startServer = require("../../index");
+const Logger = require("./library/Logger");
+
+const connectDB = async () => {
+	try {
+		await mongoose.connect(config.mongo.url, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
+		Logger.info("Connected successfully to MongoDB!\n");
+		startServer();
+	} catch (error) {
+		Logger.error("Could not connect to MongoDB", error);
+		process.exit(1);
+	}
+};
+
+module.exports = connectDB();
