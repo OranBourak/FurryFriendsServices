1a654d48c1713117f08c4eec5403cc7e4d4a07e3
Lior <Shilon199707@gmail.com>
2023-09-07 14:44:02 +0200

    (ADD) Routes handeling for Blocked Times Slots

---
+++ src/routes/BlockedTimeSlot.js
@@ -0,0 +1,13 @@
+/* eslint-disable new-cap */
+const express = require("express");
+const controller = require("../controllers/BlockedTimeSlot");
+
+const router = express.Router();
+
+router.post("/create/", controller.createBlockedTimeSlot);
+router.get("/get/:blockedTimeSlotId", controller.readBlockedTimeSlot);
+router.get("/get/", controller.readAllBlockedTimeSlots);
+router.patch("/update/:blockedTimeSlotId", controller.updateBlockedTimeSlot);
+router.delete("/delete/:blockedTimeSlotId", controller.deleteBlockedTimeSlot);
+
+export default router;
