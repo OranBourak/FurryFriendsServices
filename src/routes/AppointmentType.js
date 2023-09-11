1c5c693730d3d3fbd97e995de2e082ac8f684e18
Lior <Shilon199707@gmail.com>
2023-09-11 07:19:13 +0300

    (ADD) Add Appointment Type API routes

---
+++ src/routes/AppointmentType.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const controller = require("../controllers/AppointmentType.js");
+// eslint-disable-next-line new-cap
+const router = express.Router();
+
+router.post("/create/", controller.createAppointmentType);
+router.get("/get/:appointmentTypeId", controller.readAppointmentType);
+router.get("/get/", controller.readAllAppointmentTypes);
+router.patch("/update/:appointmentTypeId", controller.requireAuth, controller.updateAppointmentType);
+router.delete("/delete/:appointmentTypeId", controller.deleteAppointmentType);
+
+module.exports = router;
