0be74675b13568689f31fb84ec955b979da611bd
Lior <Shilon199707@gmail.com>
2023-09-12 06:19:06 +0300

    (ADD) Add routes for getting the availability managment data from backend and fo blocking a date in DB

--- src/routes/ServiceProvider.js
+++ src/routes/ServiceProvider.js
@@ -5,8 +5,10 @@ const router = new express.Router();
 
 router.post("/create/", controller.createServiceProvider);
 router.post("/login", controller.loginServiceProvider);
+router.post("/blockDate/:serviceProviderId", controller.blockDate);
 router.get("/pass-recovery", controller.getSecurityInfo);
 router.get("/get/:serviceProviderId", controller.requireAuth, controller.readServiceProvider);
+router.get("/getAvailabilityManagmentData/:serviceProviderId", controller.requireAuth, controller.getAvailabilityManagmentData);
 router.get("/getAppointments/:serviceProviderId", controller.requireAuth, controller.getAppointments);
 router.get("/getAppointmentTypes/:serviceProviderId", controller.requireAuth, controller.getAppointmentTypes);
 router.get("/get/", controller.readAllServiceProviders);
