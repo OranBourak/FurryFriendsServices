abf5805153cabd3bd5e8fe444e11c08c4cb9827e
Lior <Shilon199707@gmail.com>
2023-09-13 10:21:58 +0300

    (ADD) Add Route for grtting all the appointments of a service provider that were hold in the 5 past months (relative to the current month).

--- src/controllers/Appointment.js
+++ src/controllers/Appointment.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 const Appointment = require("../models/Appointment");
 const Client = require("../models/Client");
 const ServiceProvider = require("../models/ServiceProvider");
@@ -62,7 +63,76 @@ const cancelAppointment = async (req, res) => {
     }
 };
 
+const getPast5MonthsAppointments = async (req, res) => {
+    console.log("In getPast5MonthsAppointments");
+    const serviceProviderId = req.params.serviceProviderId;
+    // Calculate the date range: 5 months ago from the current month and the current month
+    const currentDate = new Date();
+    const fiveMonthsAgo = new Date(currentDate);
+    fiveMonthsAgo.setMonth(fiveMonthsAgo.getMonth() - 5);
+
+    // Calculate the start and end of the current month
+    const fiveMonthsAgoMonthStart = new Date(fiveMonthsAgo.getFullYear(), fiveMonthsAgo.getMonth(), 1);
+    const currentMonthEnd = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0);
+    ServiceProvider.aggregate([
+        {
+            $match: {
+                // eslint-disable-next-line new-cap
+                _id: new mongoose.Types.ObjectId(serviceProviderId),
+            },
+        },
+        {
+            $lookup: {
+                from: "appointments",
+                localField: "appointments",
+                foreignField: "_id",
+                as: "appointments",
+            },
+        },
+        {
+            $unwind: "$appointments",
+        },
+        {
+            $lookup: {
+                from: "appointmenttypes", // Replace with the actual name of your AppointmentType collection
+                localField: "appointments.appointmentType",
+                foreignField: "_id",
+                as: "appointments.appointmentType",
+            },
+        },
+        {
+            $match: {
+                "appointments.date": {
+                    $gte: fiveMonthsAgoMonthStart,
+                    $lte: currentMonthEnd,
+                },
+            },
+        },
+        {
+            $group: {
+                _id: "$_id",
+                name: {$first: "$name"}, // Add any other fields you want to retrieve from the service provider
+                email: {$first: "$email"}, // Add any other fields you want to retrieve from the service provider
+                // Add any other fields you want to retrieve from the service provider
+                appointments: {$push: "$appointments"},
+            },
+        },
+    ])
+        .exec()
+        .then((result) => {
+            // The filtered appointments with populated appointmentType will be in result[0].appointments
+            const filteredApps = result[0].appointments;
+            console.log("app 5 months ago:" + filteredApps);
+            console.log(filteredApps[0].date);
+            console.log(filteredApps[0].appointmentType[0].name);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+};
+
 module.exports = {
     createAppointment,
     cancelAppointment,
+    getPast5MonthsAppointments,
 };
--- src/routes/Appointment.js
+++ src/routes/Appointment.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 
 router.post("/createAppointment/", controller.createAppointment);
+router.get("/getPast5MonthsAppointments/:serviceProviderId", controller.getPast5MonthsAppointments);
 // router.get("/get/:appointmentId", controller.readAppointment);
 // router.get("/get/", controller.readAllAppointments);
 // router.patch("/update/:appointmentId", controller.updateAppointment);
