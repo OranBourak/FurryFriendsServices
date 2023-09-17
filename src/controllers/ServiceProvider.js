978ee7369635d794648e5f1f3250330d9a94cd56
Lior <Shilon199707@gmail.com>
2023-09-17 17:26:04 +0300

    (REF) When updating appointemnt status to completed checks if the status of the appointment is Upcoming

--- src/controllers/ServiceProvider.js
+++ src/controllers/ServiceProvider.js
@@ -95,7 +95,7 @@ const getAppointments = async (req, res) => {
             israeliCurrDateTime = new Date(appointment.date);
             israeliCurrDateTime.setMinutes(israeliCurrDateTime.getMinutes() - 180);
             israeliCurrDateTime.setHours(israeliCurrDateTime.getHours() + appointment.duration);
-            if (israeliCurrDateTime < currentDate && appointment.status !== "Completed" && appointment.status !== "Canceled") {
+            if (israeliCurrDateTime < currentDate && appointment.status === "Upcoming") {
                 // The appointment's date and time have passed; update status to "Completed"
                 appointment.status = "Completed";
                 await appointment.save();
